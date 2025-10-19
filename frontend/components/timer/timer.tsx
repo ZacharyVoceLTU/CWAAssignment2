44989b7ba612fb7e97fbb70de4363bcc16ebf02f
ZacharyVoceLTU <20731993@students.latrobe.edu.au>
2025-10-19 09:33:17 +0000

    timer now only takes input

--- frontend/components/timer/timer.tsx
+++ frontend/components/timer/timer.tsx
@@ -10,40 +10,10 @@ interface TimerProps {
 
 // Update component signature
 const Timer: React.FC<TimerProps> = ({ initialTime, setInitialTime }) => {
-    // initialTime is now a prop
-    const [remainingTime, setRemainingTime] = useState<number>(0);
-    const [isTimerRunning, setIsTimerRunning] = useState<boolean>(false);
-
-    useEffect(() => {
-        let interval: NodeJS.Timeout | null = null;
-
-        if (isTimerRunning && remainingTime > 0) {
-            interval = setInterval(() => {
-                setRemainingTime(prevSeconds => prevSeconds - 1);
-            }, 1000);
-        } else if (remainingTime <= 0 && isTimerRunning) {
-            // Timer stopped in the builder UI
-            setIsTimerRunning(false);
-        }
-
-        return () => {
-            if (interval) {
-                clearInterval(interval);
-            }
-        };
-    }, [isTimerRunning, remainingTime]);
-
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         // Use the prop setter to update the state in the parent (page.tsx)
         setInitialTime(Number(e.target.value));
     }
-
-    const startTimer = () => {
-        if (!isTimerRunning && initialTime > 0) {
-            setRemainingTime(initialTime);
-            setIsTimerRunning(true);
-        }
-    };
 
     return (
         <div>
@@ -53,10 +23,6 @@ const Timer: React.FC<TimerProps> = ({ initialTime, setInitialTime }) => {
                 onChange={handleInputChange}
                 placeholder="Set time in seconds"
             />
-            <button onClick={startTimer}>
-                Start Timer
-            </button>
-            <h2>Time Remaining: {remainingTime}s</h2>
         </div>
     )
 }
